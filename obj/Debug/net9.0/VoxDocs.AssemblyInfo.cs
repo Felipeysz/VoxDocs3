0eed35b656b89adb1f9f58ed3e4f81e4168dc61a
Felipeysz <Felipesantana87146520@gmail.com>
2025-06-16 23:02:18 -0300

    Teste

--- obj/Debug/net9.0/VoxDocs.AssemblyInfo.cs
+++ obj/Debug/net9.0/VoxDocs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoxDocs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9b6997656bd44611c9d19c79bcd63dfb349224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed14b4da4bcff911d33e71fb02fc281fb20ecdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoxDocs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoxDocs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
